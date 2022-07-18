2c2d3cc0aeaa1c8a76f34cded423c1090e14f1a7
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-07-18 14:08:36 +0200

    First JS housekeeping

--- heropicker.js
+++ heropicker.js
@@ -1,3 +1,26 @@
 /* Here the fun begins. */
 
-var role = "";
+// Static Vars // 
+const btnRand = document.getElementById("btnRand");
+var heroName = "";
+var heroImg = document.getElementById("heroImg");
+var heroDesc = document.getElementById("heroDesc");
+var heroAttrib = "";
+
+
+// Functions //
+
+function selectHeroRandom(){
+
+}
+
+
+function buttonRandom(){
+    selectHeroRandom();
+}
+
+
+btnRand.addEventListener("click", buttonRandom());
+
+
+
