bc35c7553ef189dd7919f51a7cab6e19c99993fc
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-07-19 09:49:33 +0200

    Got the list of objects working!

--- heropicker.js
+++ heropicker.js
@@ -6,7 +6,7 @@
 //Heroes// Warning HUEG!!!
 var heroes = [
     {
-        "name": "Abaddon",
+        name: "Abaddon",
         "desc": "Able to transform enemy attacks into self-healing, Abaddon can survive almost any assault. Shielding allies and launching his double-edged coil at a friend or foe, he is always ready to ride into the thick of battle.",
         "portrait": "images/abaddon.png",
         "role": "support",
@@ -14,7 +14,7 @@ var heroes = [
         "complexity": "0"
     },
     {
-        "name": "Alchemist",
+        name: "Alchemist",
         "desc": "Synthesizing extra resources from each and every kill, Alchemist has no trouble gathering the tools needed to destroy his foes. Ambushing enemies with corrosive acid and a host of unstable chemicals, he battles to ensure his greedy escapades can remain uninterrupted.",
         "portrait": "images/alchemist.png",
         "role": "carry",
@@ -23,15 +23,15 @@ var heroes = [
     }
     ,
     {
-        "name": "Ancient Apperition",
+        name: "Ancient Apperition",
         "desc": "Able to launch a powerful blast of damaging ice across the battlefield, Ancient Apparition threatens to shatter weakened enemies wherever they are. Slowing enemies as he enhances his allies' effectiveness in battle, he is a constant threat to his foes.",
-        "portrait": "images/ancientapperition.png",
+        "portrait": "images/ancientapparition.png",
         "role": "hardsupport",
         "attribute": "intelligence",
         "complexity": "1"
     },
     {
-        "name": "Anti Mage",
+        name: "Anti Mage",
         "desc": "Should Anti-Mage have the opportunity to gather his full strength, few can stop his assaults. Draining mana from enemies with every strike or teleporting short distances to escape an ambush, cornering him is a challenge for any foe.",
         "portrait": "images/antimage.png",
         "role": "carry",
@@ -39,7 +39,7 @@ var heroes = [
         "complexity": "0"
     },
     {
-        "name": "Arc Warden",
+        name: "Arc Warden",
         "desc": "A splintered fragment of the same primordial power as the Ancients themselves, Zet the Arc Warden has pledged to see the clash between Radiant and Dire finally end. Assault lone enemies with fluxing energy, or distort space to generate a protective field around allies. Summon a Spark Wraith to patrol an area for enemies to infuse with harmful magic, then create a double of Zet, items and all, to overwhelm your foes.",
         "portrait": "images/arcwarden.png",
         "role": "mid",
@@ -47,7 +47,7 @@ var heroes = [
         "complexity": "2"
     },
     {
-        "name": "Axe",
+        name: "Axe",
         "desc": "One after another, Axe cuts down his foes. Marching ahead of his team, he locks his enemies in battle then counters their blows with a deadly spin of his weapon. Slamming his culling blade through a weakened enemy, he is always charging onward.",
         "portrait": "images/axe.png",
         "role": "offlane",
@@ -55,7 +55,7 @@ var heroes = [
         "complexity": "0"
     },
     {
-        "name": "Bane",
+        name: "Bane",
         "desc": "Bane brings terror to his enemies with his arsenal of disabling abilities. Whether trapping a foe within a contagious nightmare, or gripping an enemy in place, he gives allies all the time they need to slay their enemy.",
         "role": "carry",
         "portrait": "images/bane.png",
@@ -63,7 +63,7 @@ var heroes = [
         "complexity": "1"
     },
     {
-        "name": "Bat Rider",
+        name: "Bat Rider",
         "desc": "Once he takes to the skies, Batrider can strike from any direction. Snatching a foe with his lasso, he soars above the treetops, pulling his prey through a trail of flame as he drags them before his merciless allies.",
         "portrait": "images/batrider.png",
         "role": "offlane",
@@ -71,7 +71,7 @@ var heroes = [
         "complexity": "2"
     },
     {
-        "name": "Beastmaster",
+        name: "Beastmaster",
         "desc": "Summoning a poison-spitting boar and a scouting hawk to aid his hunt, Beastmaster never stalks the battlefield unprepared. Chopping through the forest with his axes, his stunning roar opens the way for his allies to devastate enemies.",
         "portrait": "images/beastmaster.png",
         "role": "offlane",
@@ -79,10 +79,10 @@ var heroes = [
         "complexity": "1"
     },
     {
-        "name": "Bloodseeker",
+        name: "Bloodseeker",
         "desc": "Bloodseeker forces difficult decisions on his enemies. Able to drive his foes to a retreat by bathing a large area with a damaging ritual, his gruesome ultimate asks his opponents to hold still, or die.",
         "portrait": "images/bloodseeker.png",
-        "role": "midl",
+        "role": "mid",
         "attribute": "agility",
         "complexity": "0"
     }
@@ -101,16 +101,21 @@ var heroDesc = document.getElementById("heroDesc");
 // Functions //
 
 function buttonRandom(){;
-    var randomNumber = Math.floor(Math.random() * heroes.length -1);
+    var randomNumber = Math.floor(Math.random() * heroes.length);
     var randomHero = heroes[randomNumber];
     populateText(randomHero);
     console.log(randomHero);
 }
 
 function populateText(randomHero){
-  //  heroName.innerHTML = randomHero[name]
+    console.log(randomHero.name);
+    heroName.innerHTML = randomHero.name;
+    heroDesc.innerHTML = randomHero.desc;
+    heroImg.src = randomHero.portrait;
+    heroRole.innerHTML = randomHero.role;
+    heroAttribute.innerHTML = randomHero.attribute;
 };
 
-btnRand.addEventListener("click", buttonRandom());
+btnRand.addEventListener("click", buttonRandom);
 
 
